20d9f12e77c34d0a4bb46d39889acb4100db0a6d
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2015-05-04 17:18:37 -0400

    First commit.

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store.dragon.computer/admin/');
+define('HTTP_CATALOG', 'http://store.dragon.computer/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store.dragon.computer/admin/');
+define('HTTPS_CATALOG', 'http://store.dragon.computer/');
+
+// DIR
+define('DIR_APPLICATION', '/home/admin/web/store.dragon.computer/public_html/admin/');
+define('DIR_SYSTEM', '/home/admin/web/store.dragon.computer/public_html/system/');
+define('DIR_LANGUAGE', '/home/admin/web/store.dragon.computer/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/admin/web/store.dragon.computer/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/admin/web/store.dragon.computer/public_html/system/config/');
+define('DIR_IMAGE', '/home/admin/web/store.dragon.computer/public_html/image/');
+define('DIR_CACHE', '/home/admin/web/store.dragon.computer/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/admin/web/store.dragon.computer/public_html/system/download/');
+define('DIR_UPLOAD', '/home/admin/web/store.dragon.computer/public_html/system/upload/');
+define('DIR_LOGS', '/home/admin/web/store.dragon.computer/public_html/system/logs/');
+define('DIR_MODIFICATION', '/home/admin/web/store.dragon.computer/public_html/system/modification/');
+define('DIR_CATALOG', '/home/admin/web/store.dragon.computer/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin_store');
+define('DB_PASSWORD', 'l5HtpzfBMS');
+define('DB_DATABASE', 'admin_dragonstore');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store.dragon.computer/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store.dragon.computer/');
+
+// DIR
+define('DIR_APPLICATION', '/home/admin/web/store.dragon.computer/public_html/catalog/');
+define('DIR_SYSTEM', '/home/admin/web/store.dragon.computer/public_html/system/');
+define('DIR_LANGUAGE', '/home/admin/web/store.dragon.computer/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/admin/web/store.dragon.computer/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/admin/web/store.dragon.computer/public_html/system/config/');
+define('DIR_IMAGE', '/home/admin/web/store.dragon.computer/public_html/image/');
+define('DIR_CACHE', '/home/admin/web/store.dragon.computer/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/admin/web/store.dragon.computer/public_html/system/download/');
+define('DIR_UPLOAD', '/home/admin/web/store.dragon.computer/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/admin/web/store.dragon.computer/public_html/system/modification/');
+define('DIR_LOGS', '/home/admin/web/store.dragon.computer/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin_store');
+define('DB_PASSWORD', 'l5HtpzfBMS');
+define('DB_DATABASE', 'admin_dragonstore');
+define('DB_PREFIX', 'oc_');
